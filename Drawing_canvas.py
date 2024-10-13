ff8c098447ec6bbb8d755c6db260a606b9337fc0
Harshita Shreevastav <hshrtv1101@gmail.com>
2024-10-13 14:32:37 +0545

    Color bar with 5 width choose option added on left side of the canvas

--- Drawing_canvas.py
+++ Drawing_canvas.py
@@ -1,33 +1,57 @@
 import turtle
 
+# Initializations
 drawing= True
 win= turtle.Screen()
 win.title("Canvas")
 win.setup(width=1.0, height= 1.0)
 
-colors=["red", "green", "blue", "yellow", "orange", "pink", "Black", "White", "cyan", ]
-
-
+canvas= win.getcanvas()
 
 cursor= turtle.Turtle()
 cursor.width(5)
 cursor.speed(100)
 
+
+
+colors=[["red", "DarkRed"],
+        ["green", "DarkGreen"],
+        ["blue", "DeepSkyBlue"],
+       [ "yellow", "DarkGoldenrod"],
+        ["orange", "coral"],
+        ["pink", "DeepPink"],
+        ["grey", "DarkGrey"],
+        ["DarkOrchid", "DarkViolet"],
+        ["brown","chocolate"],
+        ["aquamarine", "DarkSeaGreen"],
+        ["AliceBlue", "cornsilk"],
+        ["DarkSalmon", "cyan"],
+        ["White", "Black"]]
+
+
+
+
+
+
+#Function to create color dots for color selection ------------
+
 def color_dot(x, pos):
     cursor.penup()
     cursor.goto(pos)
     cursor.pendown()
-    cursor.dot(30,"Black")
-    cursor.dot(27, x)
+    # cursor.dot(30,"Black")
+    cursor.dot(25, x)
 
 
-canvas= win.getcanvas()
 
+#Fuction that tell the cursor to START DRAWING AGAIN when the left mouse button is clicked
 
 def continue_drawing():
     global drawing
     drawing = True
     canvas.bind("<ButtonPress-1>", on_click)
+
+#Function that decreases or increases the cursor width on scrolling
 
 def width_select(event):
     if event.delta>0:
@@ -40,11 +64,13 @@ def width_select(event):
             new_width= cursor.width()-1
             cursor.width(new_width)
 
+#Function to stop drawing when mouse left button is released
 def on_release(event):
     cursor.penup()
     drawing=False
     continue_drawing()
 
+#Fuction that tell the cursor to START DRAWING when the left mouse button is clicked
 
 def on_click(event):
     x = event.x - win.window_width() / 2  # Adjust for window center
@@ -56,39 +82,90 @@ def on_click(event):
     
     canvas.bind("<Motion>", draw)
 
-# def left_draw():
-#     new_heading= cursor.heading()+20
-#     cursor.setheading(new_heading)
-#     cursor.fd(20)
 
-# def right_draw():
-#     new_heading= cursor.heading()-20
-#     cursor.setheading(new_heading)
-#     cursor.fd(20)
-
-# def up_draw():
-#     cursor.setheading(90)
-#     cursor.fd(20)
-
-# def down_draw():
-#     cursor.setheading(270)
-#     cursor.fd(20)
+#Function that tracks the mouse movement and follows it -------
 
 def draw(event):
     x = event.x - win.window_width() / 2  # Adjust for window center
     y = win.window_height() / 2 - event.y  # Adjust for window center
     cursor.goto(x, y)  # Move the turtle to the mouse position
     canvas.bind("<MouseWheel>", width_select)
+
+#Customizing Canvas -----
+
+# Placing the color selection bar
+
+color_bar=100
+cursor.penup()
+cursor.goto(-625,-200)
+cursor.pendown() 
+cursor.width(2)
+cursor_head=0
+
+#Outer color border
+
+for _ in range(2):
+    cursor.fd(color_bar)
+    cursor_head+=90
+    cursor.setheading(cursor_head)
+    cursor.fd(5*color_bar)
+    cursor_head+=90
+    cursor.setheading(cursor_head)
+
+
+#The colar dots
+
+cursor.penup()
+cursor.goto(-595, 280)
+cursor.pendown()
+
+for color in colors:
+
     
+        color_dot(color[0], cursor.pos())
+
+        x= cursor.xcor()
+        y=cursor.ycor()
+
+        cursor.penup()
+        cursor.goto(x+40,y)
+        cursor.pendown()
+
+        color_dot(color[1 ], cursor.pos())
+        
+        cursor.penup()
+        cursor.goto(x,y-30)
+        cursor.pendown()
+
+# Cursor width
+
+cursor.penup()
+cursor.goto(x-8,y-40)
+cursor.pendown()
+cursor.setheading(0)
+wid= 10
+for _ in range(5):
+
+    x= cursor.xcor()
+    y=cursor.ycor()
+    cursor.width(wid)
+    cursor.fd(60)
+    wid-=2
+
+    cursor.penup()
+    cursor.goto(x,y-15)
+    cursor.pendown()
 
 
+
+        
+
+
+#  for user interaction--------------------
 win.listen()
 canvas.bind("<ButtonPress-1>", on_click)
 canvas.bind("<ButtonRelease-1>", on_release)
-# win.onkey(up_draw,"Up")
-# win.onkey(left_draw,"Left")
-# win.onkey(down_draw,"Down")
-# win.onkey(right_draw,"Right")
+
 
 
 win.mainloop()
