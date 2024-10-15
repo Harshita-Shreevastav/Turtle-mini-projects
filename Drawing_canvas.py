6ba6e241fd9153f8e49baaa8d4bff9485fa5652e
Harshita Shreevastav <hshrtv1101@gmail.com>
2024-10-15 20:53:58 +0545

    Color selection done

--- Drawing_canvas.py
+++ Drawing_canvas.py
@@ -1,16 +1,24 @@
 import turtle
+import numpy as np
 
 # Initializations
 drawing= True
 win= turtle.Screen()
 win.title("Canvas")
-win.setup(width=1.0, height= 1.0)
+# win.setup(width=1.0, height= 1.0)
 
 canvas= win.getcanvas()
 
 cursor= turtle.Turtle()
+cursor.hideturtle()
+turtle.tracer(0)
 cursor.width(5)
 cursor.speed(100)
+
+
+dot_pos={}
+pre_pos = (0,0) 
+pre_key = ' ' 
 
 
 
@@ -35,20 +43,52 @@ colors=[["red", "DarkRed"],
 
 #Function to create color dots for color selection ------------
 
+
+
+
+def color_check(x,y):
+    print("Just Entered into color_check func")
+
+    
+    for keys in dot_pos:
+        pos=dot_pos[keys]
+        
+        if cursor.distance(pos)<13:
+            print(f"pencolor: {keys}")
+            cursor.pencolor(keys)
+            
+
+           
+       
+            
+          
+            
+            
+            break
+            
+    print("Exiting color_check")
+
+
+
 def color_dot(x, pos):
     cursor.penup()
     cursor.goto(pos)
     cursor.pendown()
-    # cursor.dot(30,"Black")
-    cursor.dot(25, x)
+    cursor.dot(25,"Black")
+    cursor.dot(20, x)
+    dot_pos[x]=cursor.pos()
 
 
 
 #Fuction that tell the cursor to START DRAWING AGAIN when the left mouse button is clicked
 
 def continue_drawing():
+    print("Just entered into continue_drawing")
+    print("Drawing = False")
     global drawing
     drawing = True
+    print("Drawing = True")
+    print("IExiting continue_drawing")
     canvas.bind("<ButtonPress-1>", on_click)
 
 #Function that decreases or increases the cursor width on scrolling
@@ -66,18 +106,30 @@ def width_select(event):
 
 #Function to stop drawing when mouse left button is released
 def on_release(event):
+    print("Just Entered into on_release func")
     cursor.penup()
     drawing=False
+    print("Drawing = False")
+    print("Exiting on_release")
     continue_drawing()
 
 #Fuction that tell the cursor to START DRAWING when the left mouse button is clicked
 
 def on_click(event):
+    print('Entered into on_click fund')
     x = event.x - win.window_width() / 2  # Adjust for window center
     y = win.window_height() / 2 - event.y 
+
     cursor.penup() # Adjust for window center
     cursor.goto(x, y) 
     cursor.pendown() # Move the turtle to the mouse position
+    
+    print(cursor.pos())
+    print("Going inside color_check func")
+    color_check(x,y)
+
+    
+    print("Back Into on_click")    
     canvas.bind("<MouseWheel>", width_select)
     
     canvas.bind("<Motion>", draw)
@@ -86,10 +138,19 @@ def on_click(event):
 #Function that tracks the mouse movement and follows it -------
 
 def draw(event):
+
+    
     x = event.x - win.window_width() / 2  # Adjust for window center
     y = win.window_height() / 2 - event.y  # Adjust for window center
+
+    
+
     cursor.goto(x, y)  # Move the turtle to the mouse position
+
+    
     canvas.bind("<MouseWheel>", width_select)
+
+    
 
 #Customizing Canvas -----
 
@@ -97,7 +158,7 @@ def draw(event):
 
 color_bar=100
 cursor.penup()
-cursor.goto(-625,-200)
+cursor.goto(-625,-175)
 cursor.pendown() 
 cursor.width(2)
 cursor_head=0
@@ -108,7 +169,7 @@ for _ in range(2):
     cursor.fd(color_bar)
     cursor_head+=90
     cursor.setheading(cursor_head)
-    cursor.fd(5*color_bar)
+    cursor.fd(4*color_bar+60)
     cursor_head+=90
     cursor.setheading(cursor_head)
 
@@ -116,14 +177,15 @@ for _ in range(2):
 #The colar dots
 
 cursor.penup()
-cursor.goto(-595, 280)
+cursor.goto(-595, 270)
 cursor.pendown()
-
+x= cursor.xcor()
+y=cursor.ycor()
 for color in colors:
 
     
         color_dot(color[0], cursor.pos())
-
+        
         x= cursor.xcor()
         y=cursor.ycor()
 
@@ -131,34 +193,28 @@ for color in colors:
         cursor.goto(x+40,y)
         cursor.pendown()
 
-        color_dot(color[1 ], cursor.pos())
+        color_dot(color[1], cursor.pos())
         
         cursor.penup()
         cursor.goto(x,y-30)
         cursor.pendown()
 
-# Cursor width
 
-cursor.penup()
-cursor.goto(x-8,y-40)
-cursor.pendown()
-cursor.setheading(0)
-wid= 10
-for _ in range(5):
 
-    x= cursor.xcor()
-    y=cursor.ycor()
-    cursor.width(wid)
-    cursor.fd(60)
-    wid-=2
+# print(dot_pos)
 
-    cursor.penup()
-    cursor.goto(x,y-15)
-    cursor.pendown()
+# dot_array=np.array(dot_pos.values())
+# print(f"Array: {dot_array}")
 
 
 
-        
+
+
+
+turtle.update()
+turtle.tracer(1)
+cursor.showturtle() 
+cursor.speed(0) 
 
 
 #  for user interaction--------------------
